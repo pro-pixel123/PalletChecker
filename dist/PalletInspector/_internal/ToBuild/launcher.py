290954daef755ba3e8416f4c4705d1ae2ee5c874
pro-pixel123 <61485020+pro-pixel123@users.noreply.github.com>
2026-04-25 21:05:19 +0200

    Delete dist/PalletInspector directory

--- dist/PalletInspector/_internal/ToBuild/launcher.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import sys
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-app = os.path.join(BASE_DIR, "PalletSiteWithlaser.py")
-
-subprocess.run([sys.executable, "-m", "streamlit", "run", app])
